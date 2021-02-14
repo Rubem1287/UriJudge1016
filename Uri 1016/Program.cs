d1da8a56028af68720c72d70e2d61094ffc27a98
Rubem <40901612+Rubem1287@users.noreply.github.com>
2021-02-14 18:56:02 -0300

    Add files via upload

---
+++ Uri 1016/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Uri_1016
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y, a;
+
+            x = 60;
+            y = 90;
+
+            Console.WriteLine();
+            a = int.Parse(Console.ReadLine());
+
+            a = a * 2;
+
+            Console.WriteLine();
+            Console.WriteLine(a+" Minutos");
+
+        }
+    }
+}
